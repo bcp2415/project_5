dee1c12669d1d873de0f21b076142247ef603033
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-08-11 15:14:11 -0500

    Adds 6 objects in js to create 6 cards

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Declare variables
+var cardWrapper = document.querySelector("#cardWrapper");
+var scoreWrapper = document.querySelector("#scoreWrapper");
+var score = 0;
+
+// Create 6 cards
+let card1 = { 1, 'elephant' };
+let card2 = { 2, 'elephant' };
+let card3 = { 3, 'tiger' };
+let card4 = { 4, 'tiger' };
+let card5 = { 5, 'penguin' };
+let card6 = { 6, 'penguin' };
+
+
+
+// Initialize game:  distribute cards randomly, display on screen, set function to run on clicking any card
+function init() {
+    // Assign an animal to each of the 6 cards
+    // Display the 6 cards in 2 columns of 3 each
+    // Set an eventListener on each card
+}
+
+//
+init();
