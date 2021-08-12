99fa4e079df1c200f3975b1cae220d606faada20
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-08-12 16:35:45 -0500

    Adds code to generate random array of 6 cards containing 2 of each animal.

--- script.js
+++ script.js
@@ -7,9 +7,36 @@ var cards = [];
 // Initialize game:  distribute cards randomly, display on screen, set function to run on clicking any card
 function init() {
     // Assign an animal to each of the 6 cards
+
+    // Set total number of each animal for final set:
+    var elephants = 2;
+    var tigers = 2;
+    var penguins = 2;
+
     // Choose animal for first card to be added:
-    var randomNum = Math.floor(Math.random() * 3);
-    console.log(randomNum);
+    while (cards.length < 6) {
+        var randomNum = Math.floor(Math.random() * 3);
+        if (randomNum == 0) {
+            if (elephants > 0) {
+                cards.push("elephant");
+                elephants = elephants - 1;
+            }
+        } else if (randomNum == 1) {
+            if (tigers > 0) {
+                cards.push("tiger");
+                tigers = tigers - 1;
+            }
+        } else if (randomNum == 2) {
+            if (penguins > 0) {
+                cards.push("penguin");
+                penguins = penguins - 1;
+            }
+        }
+        console.log(cards[0]);
+        console.log(elephants, tigers, penguins);
+        console.log(cards.length);
+        console.log(cards);
+    }
 
     // Display the 6 cards in 2 columns of 3 each
     var cardCount = 1;
