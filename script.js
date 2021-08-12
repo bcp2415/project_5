f9078b074134e6115bc6c6a6279b2501ae1ea4e9
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-08-12 16:25:06 -0500

    Starts adding fcn to randomly assign animals to the 6 cards, w/ constraints that there must a pair of each animal in final set, and each choice must be random except the last.

--- script.js
+++ script.js
@@ -2,18 +2,15 @@
 var cardWrapper = document.querySelector("#cardWrapper");
 var scoreWrapper = document.querySelector("#scoreWrapper");
 var score = 0;
-
-// Create 6 cards
-let card1 = { position: 1, animal: "elephant" };
-let card2 = { position: 2, animal: "elephant" };
-let card3 = { position: 3, animal: "tiger" };
-let card4 = { position: 4, animal: "tiger" };
-let card5 = { position: 5, animal: "penguin" };
-let card6 = { position: 6, animal: "penguin" };
+var cards = [];
 
 // Initialize game:  distribute cards randomly, display on screen, set function to run on clicking any card
 function init() {
     // Assign an animal to each of the 6 cards
+    // Choose animal for first card to be added:
+    var randomNum = Math.floor(Math.random() * 3);
+    console.log(randomNum);
+
     // Display the 6 cards in 2 columns of 3 each
     var cardCount = 1;
     while (cardCount <= 6) {
@@ -24,7 +21,7 @@ function init() {
         cardCount++;
     }
 
-    // Set an eventListener on each card
+    // Set an eventListener on each card; randomly assign the 6 card objects to the 6 cards displayed
 }
 
 //
