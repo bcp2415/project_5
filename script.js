4afbbcb3811a416adb5f955ea3e3a1e07f4f6956
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-08-29 14:47:02 -0500

    adds init fcn, genCards fcn, animalPicker fcn, all just begun

--- script.js
+++ script.js
@@ -2,3 +2,27 @@
 const cardWrapper = document.querySelector("#cardWrapper");
 const scoreWrapper = document.querySelector("#scoreWrapper");
 cards = [];
+
+function init() {
+  console.log("init fcn called.");
+
+  // generate cards, store each as an object in the cards array
+  genCards();
+}
+
+function genCards() {
+  let counter = 0;
+  while (counter < 6) {
+    const animal = animalPicker();
+    console.log(
+      `Now generating card ${counter + 1}...with an ${animal} on it.`
+    );
+    counter++;
+  }
+}
+
+function animalPicker() {
+  return "elephant";
+}
+
+init();
